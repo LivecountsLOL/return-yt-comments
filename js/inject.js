1b7d89bf56db762216184c1d28c254c9f119814b
Matt <hexzdmusic@hexzdmusic.com>
2023-10-25 21:47:42 -0400

    initial push

---
+++ js/inject.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+var s = document.createElement("script");
+s.type = "text/javascript";
+s.src = "https://cors.livecounts.lol/https://livecounts.lol/ytreturncomments.js";
+$("head").append(s);
+}
